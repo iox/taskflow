9dee5e0a9a11266aa2cd427675ebb0ebe2ba44bc
Ignacio Huerta <ignacio@ihuerta.net>
2012-09-13 09:22:36 +0200

    Update Gemfile hobo version

--- config/hobo_routes.rb
+++ config/hobo_routes.rb
@@ -5,7 +5,47 @@
 Taskflow2::Application.routes.draw do
 
 
-  # Resource routes for controller "tasks"
+  # Resource routes for controller tasks
+  resources :tasks
+
+
+  # Resource routes for controller users
+  resources :users, :only => [:edit, :show, :create, :update, :destroy] do
+    collection do
+      post 'signup', :action => 'do_signup'
+      get 'signup'
+    end
+    member do
+      get 'account'
+      put 'reset_password', :action => 'do_reset_password'
+      get 'reset_password'
+    end
+  end
+
+  # User routes for controller users
+  match 'login(.:format)' => 'users#login', :as => 'user_login'
+  get 'logout(.:format)' => 'users#logout', :as => 'user_logout'
+  match 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
+
+
+  # Resource routes for controller steps
+  resources :steps do
+    collection do
+      post 'reorder'
+    end
+  end
+
+
+
+# These are the Hobo 1.3 style routes.   They've been included for backwards
+# compatibility reasons because the names of some named routes have been changed.
+# Please update your views to use the new named route names and then disable this
+# section by setting config.hobo.dont_emit_deprecated_routes = true.
+
+
+ 
+
+  # DEPRECATED Resource routes for controller tasks
   get 'tasks(.:format)' => 'tasks#index', :as => 'tasks'
   get 'tasks/new(.:format)' => 'tasks#new', :as => 'new_task'
   get 'tasks/:id/edit(.:format)' => 'tasks#edit', :as => 'edit_task'
@@ -15,29 +55,27 @@ Taskflow2::Application.routes.draw do
   delete 'tasks/:id(.:format)' => 'tasks#destroy', :as => 'destroy_task', :constraints => { :id => %r([^/.?]+) }
 
 
-  # Lifecycle routes for controller "users"
+  # DEPRECATED Lifecycle routes for controller users
   post 'users/signup(.:format)' => 'users#do_signup', :as => 'do_user_signup'
   get 'users/signup(.:format)' => 'users#signup', :as => 'user_signup'
   put 'users/:id/reset_password(.:format)' => 'users#do_reset_password', :as => 'do_user_reset_password'
   get 'users/:id/reset_password(.:format)' => 'users#reset_password', :as => 'user_reset_password'
 
-  # Resource routes for controller "users"
+  # DEPRECATED Show action routes for controller users
+  get 'users/:id/account(.:format)' => 'users#account', :as => 'user_account'
+
+  # DEPRECATED Resource routes for controller users
   get 'users/:id/edit(.:format)' => 'users#edit', :as => 'edit_user'
   get 'users/:id(.:format)' => 'users#show', :as => 'user', :constraints => { :id => %r([^/.?]+) }
   post 'users(.:format)' => 'users#create', :as => 'create_user'
   put 'users/:id(.:format)' => 'users#update', :as => 'update_user', :constraints => { :id => %r([^/.?]+) }
   delete 'users/:id(.:format)' => 'users#destroy', :as => 'destroy_user', :constraints => { :id => %r([^/.?]+) }
 
-  # Show action routes for controller "users"
-  get 'users/:id/account(.:format)' => 'users#account', :as => 'user_account'
 
-  # User routes for controller "users"
-  match 'login(.:format)' => 'users#login', :as => 'user_login'
-  get 'logout(.:format)' => 'users#logout', :as => 'user_logout'
-  match 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
+  # DEPRECATED Reorder routes for controller steps
+  post 'steps/reorder(.:format)', :as => 'reorder_steps'
 
-
-  # Resource routes for controller "steps"
+  # DEPRECATED Resource routes for controller steps
   get 'steps(.:format)' => 'steps#index', :as => 'steps'
   get 'steps/new(.:format)' => 'steps#new', :as => 'new_step'
   get 'steps/:id/edit(.:format)' => 'steps#edit', :as => 'edit_step'
@@ -45,8 +83,5 @@ Taskflow2::Application.routes.draw do
   post 'steps(.:format)' => 'steps#create', :as => 'create_step'
   put 'steps/:id(.:format)' => 'steps#update', :as => 'update_step', :constraints => { :id => %r([^/.?]+) }
   delete 'steps/:id(.:format)' => 'steps#destroy', :as => 'destroy_step', :constraints => { :id => %r([^/.?]+) }
-
-  # Reorder routes for controller "steps"
-  post 'steps/reorder(.:format)', :as => 'reorder_steps'
 
 end
