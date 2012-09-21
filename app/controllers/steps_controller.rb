702aa54754b290a50b0a9c809a4b3983af8d87e5
Ignacio Huerta <ignacio@ihuerta.net>
2012-09-21 08:08:33 +0200

    Fix for creating new steps after reordering steps

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -5,24 +5,24 @@ class StepsController < ApplicationController
   auto_actions :all
   
   def create
-    hobo_create do
-      # Level 0
-      if @step.task_id
-        @task = @step.task
-        @current = @task
-        @step = nil
-      # Level 1
-      else
-        @task = @step.ancestors.last.task
-        @current = @step.parent
-        @step = @step.parent
-      end
-      ajax_update_response(
-          params[:render].values, {}, 
-          {:postamble => "
-            $('.bar').width('#{@task.completedness}%') 
-          "})
+    @step = Step.create(params[:step])
+    # Level 0
+    if @step.task_id
+      @task = @step.task
+      @current = @task
+      @step = nil
+    # Level 1
+    else
+      @task = @step.ancestors.last.task
+      @current = @step.parent
+      @step = @step.parent
     end
+#        ajax_update_response(
+#          params[:render].values, {}, 
+#          {:postamble => "
+#            $('.bar').width('#{@task.completedness}%') 
+#          "})
+#    end
   end
   
   def destroy
@@ -46,8 +46,16 @@ class StepsController < ApplicationController
     end
     logger.info '>>> Moving finished'
     @task = @step.task
-    @current = @step.parent
-    @current = @task if @step.task_id
+    if @step.task_id
+      @current = @task 
+      @moved = @step
+      @step = nil
+    else
+      @current = @step.parent
+      @moved = @step
+      @step = @step.parent
+    end
+    
   end
 
 end
